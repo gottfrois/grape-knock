1e69a9ed5272c89e1cef37d329afabe14a7c2dda
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2015-08-20 20:03:03 +0200

    add current_user helper method

--- lib/grape/knock/authenticable.rb
+++ lib/grape/knock/authenticable.rb
@@ -1,25 +1,35 @@
 require 'knock'
 require 'knock/authenticable'
 require 'grape/knock/forbidden_error'
+require 'grape/knock/methods'
 
 module Grape
   module Knock
     class Authenticable < Grape::Middleware::Base
       include ::Knock::Authenticable
 
-      def token
-        env['HTTP_AUTHORIZATION'].to_s.split(' ').last
-      end
-
-      def authenticate
-        fail ::Grape::Knock::ForbiddenError unless token
-        ::Knock::AuthToken.new(token: token).current_user
-      rescue ::JWT::DecodeError
-        fail ::Grape::Knock::ForbiddenError
+      def context
+        env['api.endpoint']
       end
 
       def before
         authenticate
+        context.extend Grape::Knock::Methods
+        context.current_user = @current_user
+      end
+
+      private
+
+      def authenticate
+        fail ::Grape::Knock::ForbiddenError unless token
+        @current_user = ::Knock::AuthToken.new(token: token).current_user
+        fail ::Grape::Knock::ForbiddenError unless @current_user
+      rescue ::JWT::DecodeError
+        fail ::Grape::Knock::ForbiddenError
+      end
+
+      def token
+        env['HTTP_AUTHORIZATION'].to_s.split(' ').last
       end
 
     end
---
+++ lib/grape/knock/methods.rb
@@ -0,0 +1,9 @@
+module Grape
+  module Knock
+    module Methods
+
+      attr_accessor :current_user
+
+    end
+  end
+end
